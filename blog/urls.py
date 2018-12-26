abbacd81da0b648596c9c27ccb2c501f74083e0c
Ekupov,Velin V.S <v.ekupov@student.fontys.nl>
2018-12-26 19:51:37 +0200

    create post with ingredients

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 
-from blog.views import create, display_post, delete_post, display_all_posts, update_post, print_post
+from blog.views import display_post, delete_post, display_all_posts, update_post, print_post, PostView
 
 urlpatterns = [
     url(r'^posts/$', display_all_posts, name="DisplayPosts"),
-    url(r'^posts/create/$', create, name="CreatePost"),
+    url(r'^posts/create/$', PostView.as_view(), name="CreatePost"),
     url(r'^post/delete/$', delete_post, name="DeletePost"),
     url(r'^post/(?P<post_id>\d+)/$', display_post, name="DisplayPost"),
     url(r'^post/(?P<post_id>\d+)/update/$', update_post, name="UpdatePost"),
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,61 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseRedirect
+from django.forms import modelformset_factory
+from django.views import View
 
-from blog.forms import BlogCreationForm, PostUpdateForm
-from blog.models import Post
+from blog.forms import BlogCreationForm, PostUpdateForm, IngredientsForm
+from blog.models import Post, Ingredient
 
 from weasyprint import HTML
 
-@login_required
-def create(request):
-    if request.method == "POST":
-        form = BlogCreationForm(request.POST, request.FILES)
-        if form.is_valid():
+# @login_required
+# def create(request):
+#     IngredientFormset = modelformset_factory(Ingredient, form=IngredientsForm)
+#     if request.method == "POST":
+#         form_blog = BlogCreationForm(request.POST, request.FILES)
+#         form_ingredient = IngredientFormset(request.POST)
+#         if form_blog.is_valid() and form_ingredient.is_valid():
+#             user = request.user
+#             post = form_blog.save(user)
+#             import pdb; pdb.set_trace()
+#             form_ingredient = form_ingredient.save(post)
+#             return redirect("/blog/post/" + str(post.id))
+#     else:
+#         form_blog = BlogCreationForm()
+#         form_ingredient = IngredientsForm()
+#     return render(request, "CreatePost.html", { "form_blog" : form_blog, "form_ingredient" : form_ingredient })
+
+class PostView(View):
+    IngredientFormset = modelformset_factory(Ingredient, form=IngredientsForm)
+
+    def get(self, request, *args, **kwargs):
+        form_blog = BlogCreationForm()
+        form_ingredient = self.IngredientFormset(queryset=Ingredient.objects.none())
+
+        return render(request, "CreatePost.html", { "form_blog" : form_blog, "form_ingredient" : form_ingredient })
+
+    def post(self, request, *args, **kwargs):
+        form_blog = BlogCreationForm(self.request.POST, request.FILES)
+        form_ingredient = self.IngredientFormset(self.request.POST, queryset=Ingredient.objects.none())
+        import pdb; pdb.set_trace()
+        if form_blog.is_valid() and form_ingredient.is_valid():
             user = request.user
-            post = form.save(user)
-            return redirect("/blog/post/" + str(post.id))
-    else:
-        form = BlogCreationForm()
-    return render(request, "CreatePost.html", { "form" : form })
+            post = form_blog.save(user)
+            
+            for ingredient in form_ingredient:
+                ingredient = ingredient.save(post)
+                
+            return HttpResponseRedirect("/blog/post/" + str(post.id))
+        
+        return render(request, "CreatePost.html", { "form_blog" : form_blog, "form_ingredient" : form_ingredient })
     
 
 def display_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-    return render(request, "PostDetails.html", { "post" : post })
+    ingredients = get_list_or_404(Ingredient, recepie=post_id)
+    return render(request, "PostDetails.html", { "post" : post, "ingredients": ingredients })
 
 @login_required
 def delete_post(request):
