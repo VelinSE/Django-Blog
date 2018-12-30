803cb3f11e3826dd216e5e0d86af6345b545c053
Aleksandar Staykov <a.staykov@student.fontys.nl>
2018-12-30 21:50:42 +0100

    social login added

--- recepie/models.py
+++ recepie/models.py
@@ -6,3 +6,12 @@ from django.db.models.signals import post_save
 class UserExtended(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.FileField(upload_to="avatar")    
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserExtended.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userextended.save()
