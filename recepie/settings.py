3524f178cd62093d72fefc6d01aa1cc3c493eff1
Aleksandar Staykov <a.staykov@student.fontys.nl>
2019-01-03 21:00:41 +0100

    static files changes

--- recepie/settings.py
+++ recepie/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'social_django',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [  
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -152,17 +152,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = '/recepie/static/'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'storage/media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'recepie/static'),
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "recepie/static/recepie"),
 )
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
