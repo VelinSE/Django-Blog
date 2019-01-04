e3a8e983863fe15e0afa57611b607096ee1de58c
Aleksandar Staykov <a.staykov@student.fontys.nl>
2019-01-04 19:19:03 +0100

    search bar fixed changed blog image to thumbnail in details

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from blog.views import display_post, delete_post, display_all_posts, print_post, PostCreateView, PostUpdateView
+from blog.views import display_post, delete_post, display_posts, print_post, PostCreateView, PostUpdateView
 
 urlpatterns = [
-    url(r'^posts/$', display_all_posts, name="DisplayPosts"),
+    url(r'^posts/$', display_posts, name="DisplayPosts"),
     url(r'^posts/create/$', PostCreateView.as_view(), name="CreatePost"),
     url(r'^post/delete/$', delete_post, name="DeletePost"),
     url(r'^post/(?P<post_id>\d+)/$', display_post, name="DisplayPost"),
