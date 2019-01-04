d33694cfbe9de78ed111e55fc36fd83b37160035
Ekupov,Velin V.S <v.ekupov@student.fontys.nl>
2019-01-04 17:46:54 +0200

    login form done

--- recepie/static/recepie/res/js/add-ingredient.js
+++ recepie/static/recepie/res/js/add-ingredient.js
@@ -1,11 +1,11 @@
-(function() {
+(function () {
   'use strict';
-  window.addEventListener('load', function() {
+  window.addEventListener('load', function () {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation');
     // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
         if (form.checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
@@ -16,9 +16,6 @@
   }, false);
 })();
 
-(function() {
-  $('#delete_button').on('click', function() {
-    console.log('ada');
-    $('#delete_form').submit();
-  })
+(function () {
+  
 })();
