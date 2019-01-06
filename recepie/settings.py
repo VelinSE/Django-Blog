1e1d44595845fa7b6e3a9e005b404e66c1fcb10e
Aleksandar Staykov <a.staykov@student.fontys.nl>
2019-01-07 00:04:10 +0100

    static

--- recepie/settings.py
+++ recepie/settings.py
@@ -152,13 +152,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = '/recepie/static/'
+#STATIC_ROOT = '/recepie/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'storage/media')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "recepie/static/recepie"),
+    os.path.join(BASE_DIR,'recepie/static'),
+    os.path.join(BASE_DIR, 'static'),
+
 )
 
 REST_FRAMEWORK = {
